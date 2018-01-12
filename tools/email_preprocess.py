b38807dae7a73e343acc4c6520d85e65affc12ea
yj881664 <33710731+yj881664@users.noreply.github.com>
2018-01-12 11:27:18 +0800

    change the email_procession to fit in with python3

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import pickle
-import cPickle
 import numpy
 
 from sklearn import cross_validation
@@ -18,23 +17,20 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
             -- splits into training/testing sets (10% testing)
             -- vectorizes into tfidf matrix
             -- selects/keeps most helpful features
-
         after this, the feaures and labels are put into numpy arrays, which play nice with sklearn functions
-
         4 objects are returned:
             -- training/testing features
             -- training/testing labels
-
     """
 
     ### the words (features) and authors (labels), already largely preprocessed
     ### this preprocessing will be repeated in the text learning mini-project
-    authors_file_handler = open(authors_file, "r")
-    authors = pickle.load(authors_file_handler)
+    authors_file_handler = open(authors_file, "rb")
+    authors = pickle.load(authors_file_handler, fix_imports=True)
     authors_file_handler.close()
 
-    words_file_handler = open(words_file, "r")
-    word_data = cPickle.load(words_file_handler)
+    words_file_handler = open(words_file, "rb")
+    word_data = pickle.load(words_file_handler,fix_imports=True)
     words_file_handler.close()
 
     ### test_size is the percentage of events assigned to the test set
@@ -59,7 +55,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     features_test_transformed  = selector.transform(features_test_transformed).toarray()
 
     ### info on the data
-    print "no. of Chris training emails:", sum(labels_train)
-    print "no. of Sara training emails:", len(labels_train)-sum(labels_train)
+    print ("no. of Chris training emails:", sum(labels_train))
+    print ("no. of Sara training emails:", len(labels_train)-sum(labels_train))
     
     return features_train_transformed, features_test_transformed, labels_train, labels_test
